b801752746ead4c8165fcd58cb879ba859570679
Ian-Garrigan <igarrigan@hotmail.com>
2023-04-06 23:08:52 +0000

    Fix migration error & add url name

--- workouts/urls.py
+++ workouts/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.my_app, name='hello'),
+    path('', views.my_app, name='add_workout'),
 ]
+
+# view should be add workout + name space. Use name for href refer
+# urlpatterns = [
+#     path('', views.my_app, name='add_workout'),
+# ]
+
