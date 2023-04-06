abc7c123e9fb046c7a3ac6de0a9cd8fe47cf8761
Ian-Garrigan <igarrigan@hotmail.com>
2023-04-06 01:15:23 +0000

    Register initial model setup

---
+++ workouts/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import WorkoutTemplate, WorkoutLog, Exercises, Tracker
+
+# Register your models here.
+
+admin.site.register(Exercises)
+admin.site.register(WorkoutTemplate)
+admin.site.register(WorkoutLog)
+admin.site.register(Tracker)
