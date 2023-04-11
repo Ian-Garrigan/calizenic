2243dae88159cfb0e5b8df45d9174c20cc886543
Ian-Garrigan <igarrigan@hotmail.com>
2023-04-11 03:48:55 +0000

    Delete initial form

--- workouts/forms.py
+++ workouts/forms.py
@@ -3,19 +3,3 @@ from django.forms import ModelForm
 
 from .models import *
 
-
-class AddWorkout(forms.ModelForm):
-    def __init__(self, user, *args, **kwargs):
-        super(AddWorkout, self).__init__(*args, **kwargs)
-        self.fields['athlete_id'].initial = user.id
-
-    class Meta:
-        model = WorkoutTemplate
-        fields = ['template_name', 'was_performed', 'athlete_id']
-
-
-class LogWorkout(forms.ModelForm):
-
-    class Meta:
-        model = WorkoutLog
-        fields = '__all__'
