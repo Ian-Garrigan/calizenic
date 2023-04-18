2fad2460c32e0f548a6f021c5df1ea10ac95dba0
Ian-Garrigan <igarrigan@hotmail.com>
2023-04-18 21:01:33 +0000

    Add new form to forms.py

--- workouts/forms.py
+++ workouts/forms.py
@@ -3,17 +3,58 @@ from django import forms
 from .models import *
 
 
-class CreateWorkoutForm(forms.ModelForm):
-    class Meta:
-        model = WorkoutLog
-        exclude = ()
-        labels = {
-            'log_name': 'Template Name',
-            'weight': 'Weight(Kg)',
-            'sets': 'Number of Sets',
-            'reps': 'Number of Reps',
-            'note': 'Workout Note',
-        }
+class CreateWorkoutTemplateForm(forms.Form):
+    template_name = forms.CharField(
+        label='Template Name',
+        max_length=50,
+        )
+    exercise = forms.ModelChoiceField(
+        queryset=Exercises.objects.all(),
+        widget=forms.Select(attrs={'id': 'exercise-select'}),
+        )
+    muscle_group = forms.CharField(
+        label='Muscle Group',
+        max_length=50,
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}),
+    )
+    weight = forms.DecimalField(
+        label='Weight(Kg)',
+        max_digits=3,
+        decimal_places=1,
+        initial=00.0,
+    )
+    sets = forms.IntegerField(
+        label='Number of Sets',
+        initial=3,
+    )
+    reps = forms.IntegerField(
+        label='Number of Reps',
+        initial=5,
+    )
+    note = forms.Textarea(
+        attrs={'rows': 6},
+        label='Note',
+        required=False,
+        initial='You can include any additional notes or comments about your workout here, '
+                'such as modifications you made to the routine or reminders for future sessions.',
+    )
+
+
+# class CreateWorkoutForm(forms.ModelForm):
+#     class Meta:
+#         model = WorkoutLog
+#         exclude = ('log_name', )
+#         labels = {
+#             # 'log_name': 'Template Name',
+#             'weight': 'Weight(Kg)',
+#             'sets': 'Number of Sets',
+#             'reps': 'Number of Reps',
+#             'note': 'Workout Note',
+#         }
+#         widgets = {
+#         'note': forms.Textarea(attrs={'rows': 6}),
+#         }
+
 
 
 class WorkoutTemplateForm(forms.ModelForm):
