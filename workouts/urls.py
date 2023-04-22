db4539c1e6ecbccb13237211e23cb0b9df89def3
Ian-Garrigan <igarrigan@hotmail.com>
2023-04-22 00:42:27 +0000

    Update url path

--- workouts/urls.py
+++ workouts/urls.py
@@ -7,10 +7,10 @@ app_name = 'workouts'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('user-dashboard/', views.UserDashboard.as_view(), name='user-dashboard'),
-    path('user-templates-list/', views.UserTemplatesList.as_view(), name='user-templates'),
-    # path('create-workout/', views.CreateWorkout.as_view(), name='create-workout'),
-    path('create-workout/<int:pk>/', views.CreateWorkoutDetail.as_view(), name='detail-create-workout'),
-    path('edit-workout/<int:id>', views.edit_workout, name='edit-workout'),
+    path('user-templates-list/', views.UserTemplatesList.as_view(), name='user-templates-list'),
+    path('create-workout/', views.create_workout_template, name='create-workout'),
+    # path('create-workout/<int:pk>/', views.CreateWorkoutDetail.as_view(), name='detail-create-workout'),
+    # path('edit-workout/<int:id>', views.edit_workout, name='edit-workout'),
     path('view-logs/<int:id>/', views.view_logs, name='view-logs'),
     path('tracker-list/', views.tracker_list, name='tracker'),
 ]
