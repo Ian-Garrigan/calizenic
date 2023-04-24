3bf1e39b2d24d6f8cc48198077fef528cd8f143f
Ian-Garrigan <igarrigan@hotmail.com>
2023-04-24 03:56:23 +0000

    Create edit log form in forms.py

--- workouts/forms.py
+++ workouts/forms.py
@@ -43,21 +43,14 @@ class DataFromWorkoutLogAndExercises(forms.ModelForm):
 
 
 class EditLogForm(forms.ModelForm):
-    exercise_dropdown = forms.ModelChoiceField(
-        queryset=Exercises.objects.all(),
-        label='Exercise Type',
-        required=True,
-        empty_label='Choose one:',
-        widget=forms.Select(attrs={'id': 'select-exercise'}),
-    )
-
+   
     class Meta:
         model = WorkoutLog
-        fields = ('exercise_type', 'weight', 'sets', 'reps', )
+        fields = ('exercise_type', 'weight', 'sets', 'reps', 'note' )
 
     def save(self, commit=True):
         log = super(EditLogForm, self).save(commit=False)
-        log.exercise_type = self.cleaned_data('exercise_dropdown')
+        log.exercise_dropdown = self.cleaned_data['exercise_type']
         if commit:
             log.save()
         return log
