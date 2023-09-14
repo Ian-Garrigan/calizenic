d370bad439ec68f5272a1d57ea99d9c5cd2c6abf
Ian-Garrigan <igarrigan@hotmail.com>
2023-09-14 09:29:59 +0000

    Create test for view rendering

--- workouts/test_views.py
+++ workouts/test_views.py
@@ -3,13 +3,14 @@ from django.urls import reverse
 
 class TestViews(TestCase):
 
-    # def test_get_home_page_renders(self):
-    #     response = self.client.get('')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'workouts/home.html')
-
+    def test_get_home_page_renders(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_get_user_dashboard_renders(self):
-        response = self.client.get('/user-dashboard')
+        # reverse function to get the url
+        url = reverse('workouts:user-dashboard')
+        response = self.client.get(url, follow=True) 
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, '/workspace/calizenic/workouts/templates/user-dashboard.html')
+        self.assertTemplateUsed(response, 'user-dashboard.html')
